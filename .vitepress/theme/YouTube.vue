7d1de78463f6f1016d681eccb0e9a7f7d74dfa4d
magnetenstad <tenstad.magne@gmail.com>
2023-10-27 01:19:21 +0200

    Add YouTube video

---
+++ .vitepress/theme/YouTube.vue
@@ -0,0 +1,10 @@
+<template #home-hero-before>
+  <iframe
+    style="height: 300px; aspect-ratio: 1.7777; z-index: 1; border-radius: 8px"
+    src="https://www.youtube.com/embed/_MLGLDGypAE?amp;&autoplay=1&playsinline=1&start=40"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  >
+  </iframe>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,15 +3,17 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import YouTube from './YouTube.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'home-hero-image': h(YouTube),
     })
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
 } satisfies Theme
