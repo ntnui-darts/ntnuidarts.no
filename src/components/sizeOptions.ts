7e945f22546e932dffdf7c5198934c102487e45b
magnetenstad <tenstad.magne@gmail.com>
2025-01-29 18:36:23 +0100

    Add shop frontend

---
+++ src/components/sizeOptions.ts
@@ -0,0 +1,53 @@
+export const manWomanSizeOptions = [
+  {
+    text: 'Man, Small',
+    value: 'man-xl',
+  },
+  {
+    text: 'Man, Medium',
+    value: 'man-medium',
+  },
+  {
+    text: 'Man, Large',
+    value: 'man-large',
+  },
+  {
+    text: 'Man, XL',
+    value: 'man-xl',
+  },
+  {
+    text: 'Woman, Small',
+    value: 'woman-xl',
+  },
+  {
+    text: 'Woman, Medium',
+    value: 'woman-medium',
+  },
+  {
+    text: 'Woman, Large',
+    value: 'woman-large',
+  },
+  {
+    text: 'Woman, XL',
+    value: 'man-xl',
+  },
+]
+
+export const unisexSizeOptions = [
+  {
+    text: 'Unisex, Small',
+    value: 'unisex-xl',
+  },
+  {
+    text: 'Unisex, Medium',
+    value: 'unisex-medium',
+  },
+  {
+    text: 'Unisex, Large',
+    value: 'unisex-large',
+  },
+  {
+    text: 'Unisex, XL',
+    value: 'man-xl',
+  },
+]
